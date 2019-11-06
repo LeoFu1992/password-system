550dd6f1cce9b6d4408818533fa98c39c8f60e36
Leo Fu <leo510032000@gmail.com>
2019-11-06 22:44:14 +0800

    third commit

---
+++ password2.py
@@ -0,0 +1,12 @@
+password = '1024'
+i = 3 #剩餘次數
+while True:
+	pwd = input('請輸入四位數密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤 還有', i, '次機會')
+		if i == 0:
+			break
